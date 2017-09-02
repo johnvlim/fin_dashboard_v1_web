8dbaf348b9f5f8abe70d633db70aa4273c89ab2c
John Victor Lim <johnvlim.jhvco@gmail.com>
2017-09-02 13:49:45 -0700

    Partial code commit - on chart testing

--- fin_dashboard_v1_web/docs/test/chart/js/controllers/testChartController.js
+++ fin_dashboard_v1_web/docs/test/chart/js/controllers/testChartController.js
@@ -6,8 +6,21 @@ angular
 		);
 
 testChartController.$inject = [
-]
+	'$scope'
+	];
 
 function testChartController(
+		$scope
 		){
+	var vm = this;
+	vm.chartLabel = [	//source: http://jtblin.github.io/angular-chart.js/
+		'Download Sales', 
+		'In-Store Sales', 
+		'Mail-Order'
+	];
+	vm.chartData = [	//source: http://jtblin.github.io/angular-chart.js/
+		300, 
+		500, 
+		100
+	];
 }
