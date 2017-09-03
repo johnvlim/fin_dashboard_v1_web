b63388c16c4f9055356038281a31f1d2b6e6e134
John Victor Lim <johnvlim.jhvco@gmail.com>
2017-09-02 17:08:38 -0700

    Partial code commit: updating directive handler

--- fin_dashboard_v1_web/docs/common/js/directives/chartDirectives.js
+++ fin_dashboard_v1_web/docs/common/js/directives/chartDirectives.js
@@ -13,6 +13,8 @@ function lineChart() {
 	dirLineChart.template = '<div>line_chart_template_test</div>';
 	dirLineChart.controller = lineChartController;
 	dirLineChart.controllerAs = 'vm';
+	dirLineChart.link = function(scope, element, attrs) {
+	}
 
 	return dirLineChart;
 }
@@ -24,27 +26,99 @@ function barChart() {
 	dirBarChart.template = '<div>bar_chart_template_test</div>';
 	dirBarChart.controller = barChartController;
 	dirBarChart.controllerAs = 'vm';
+	dirBarChart.link = function(scope, element, attrs) {
+	}
 
 	return dirBarChart;
 }
 
 function doughnutChart() {
+	var dirDoughnutChart = {};
+
+	dirDoughnutChart.restrict = 'E';
+	dirDoughnutChart.template = '<div>doughnut_chart_template_test</div>';
+	dirDoughnutChart.controller = doughnutChartController;
+	dirDoughnutChart.controllerAs = 'vm';
+	dirDoughnutChart.link = function(scope, element, attrs) {
+	}
+
+	return dirDoughnutChart;
 }
 
 function radarChart() {
+	var dirRadarChart = {};
+
+	dirRadarChart.restrict = 'E';
+	dirRadarChart.template = '<div>radar_chart_template_test</div>';
+	dirRadarChart.controller = radarChartController;
+	dirRadarChart.controllerAs = 'vm';
+	dirRadarChart.link = function(scope, element, attrs) {
+	}
+
+	return dirRadarChart;
 }
 
 function pieChart() {
+	var dirPieChart = {};
+
+	dirPieChart.restrict = 'E';
+	dirPieChart.template = '<div>pie_chart_template_test</div>';
+	dirPieChart.controller = pieChartController;
+	dirPieChart.controllerAs = 'vm';
+	dirPieChart.link = function(scope, element, attrs) {
+	}
+
+	return dirPieChart;
 }
 
 function polarAreaChart() {
+	var dirPolarAreaChart = {};
+
+	dirPolarAreaChart.restrict = 'E';
+	dirPolarAreaChart.template = '<div>polar_area_chart_template_test</div>';
+	dirPolarAreaChart.controller = polarAreaChartController;
+	dirPolarAreaChart.controllerAs = 'vm';
+	dirPolarAreaChart.link = function(scope, element, attrs) {
+	}
+
+	return dirPolarAreaChart;
 }
 
 function horizontalBarChart() {
+	var dirHorizontalBarChart = {};
+
+	dirHorizontalBarChart.restrict = 'E';
+	dirHorizontalBarChart.template = '<div>horizontal_bar_chart_template_test</div>';
+	dirHorizontalBarChart.controller = horizontalBarChartController;
+	dirHorizontalBarChart.controllerAs = 'vm';
+	dirHorizontalBarChart.link = function(scope, element, attrs) {
+	}
+
+	return dirHorizontalBarChart;
 }
 
 function bubbleChart() {
+	var dirBubbleChart = {};
+
+	dirBubbleChart.restrict = 'E';
+	dirBubbleChart.template = '<div>bubble_chart_template_test</div>';
+	dirBubbleChart.controller = bubbleChartController;
+	dirBubbleChart.controllerAs = 'vm';
+	dirBubbleChart.link = function(scope, element, attrs) {
+	}
+
+	return dirBubbleChart;
 }
 
 function dynamicChart() {
+	var dirDynamicChart = {};
+
+	dirDynamicChart.restrict = 'E';
+	dirDynamicChart.template = '<div>dynamic_chart_template_test</div>';
+	dirDynamicChart.controller = dynamicChartController;
+	dirDynamicChart.controllerAs = 'vm';
+	dirDynamicChart.link = function(scope, element, attrs) {
+	}
+
+	return dirDynamicChart;
 }
