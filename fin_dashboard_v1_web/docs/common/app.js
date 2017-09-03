f41af830e71f0ea36cb1c5afe532ee0d30fa4411
John Victor Lim <johnvlim.jhvco@gmail.com>
2017-09-03 14:29:50 -0700

    Partial code commit: updating source

--- fin_dashboard_v1_web/docs/common/app.js
+++ fin_dashboard_v1_web/docs/common/app.js
@@ -1,2 +1,14 @@
-angular.module('fin_dashboard_web', [ 'ui.bootstrap', 'ui.router', 'chart.js',
-		'angularResizable' ]);
+angular.module('fin_dashboard_web',
+		[ 'ui.bootstrap', 'ui.router', 'chart.js', 'angularResizable' ])
+		.config(doRouteConfig);
+
+function doRouteConfig($stateProvider, $urlRouterProvider) {
+	$stateProvider.state('home', {
+		url : '/home',
+		views : {
+
+		}
+	});
+
+	$urlRouterProvider.otherwise('/home');
+}
---
+++ fin_dashboard_v1_web/docs/common/top/js/controllers/topController.js
@@ -0,0 +1,6 @@
+angular.module('fin_dashboard_web').controller('topController', topController);
+
+topController.$inject = [];
+
+function topController() {
+}
