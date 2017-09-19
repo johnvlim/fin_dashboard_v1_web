99261fc93a6ebefcf3299dd8a0532540de3d0ac7
dannngz <allertseairam@hotmail.com>
2017-09-19 14:29:48 +0800

    - updated

--- fin_dashboard_v1_web/docs/common/constants.js
+++ fin_dashboard_v1_web/docs/common/constants.js
@@ -1,24 +1,19 @@
 angular
 		.module('fin_dashboard_web')
-		.constant('API_JENKINS', {
-			base : 'http://localhost:8080/jenkins'
-		})
-
-		.constant('BROADCAST_MESSAGES', {
-			doJenkinsBuild : 'doJenkinsBuild'
-		})
-
 		.constant('DIRECTIVE_RESTRICTIONS', {
 			res_attribute : 'A',
 			res_class : 'C',
 			res_element : 'E'
 		})
 
-		.constant(
-				'HTTP_REQUEST_HEADER',
-				{
-					Authorization : 'Basic YWRtaW46OWRiYjhmMTE0YWVjNDMzNDkyMmQzYmFkNzdhZjhiMjQ='
-				})
+		.constant('WS_CONFIG', {
+			host : 'localhost',
+			port : '8081'
+		})
+
+		.constant('API_JENKINS', {
+			base : 'http://man-d1q7zw02:8080/jenkins'
+		})
 
 		.constant('HTTP_REQUEST_METHOD', {
 			methodGet : 'GET',
@@ -27,7 +22,12 @@ angular
 			methodDelete : 'DELETE'
 		})
 
-		.constant('WS_CONFIG', {
-			host : 'localhost',
-			port : '8081'
-		})
+		.constant(
+				'HTTP_REQUEST_HEADER',
+				{
+					Authorization : 'Basic YWRtaW46OWRiYjhmMTE0YWVjNDMzNDkyMmQzYmFkNzdhZjhiMjQ='
+				})
+
+		.constant('BROADCAST_MESSAGES', {
+			doJenkinsBuild : 'doJenkinsBuild'
+		});
